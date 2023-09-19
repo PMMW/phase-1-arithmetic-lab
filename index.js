8da53e911fda1ad3cb43bade3602b5035db72b8c
Morrison Wanja <morrisonwanja@gmail.com>
2023-09-19 08:07:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+let num1 =31;
+let num2 =2;
+let multiply =num1*num2;
+console.log(multiply);
+
+// let random = Math.random()*10;
+// console.log(Math.floor(random) + 1);
+
+let random = Math.floor(Math.random() *10) + 1;
+console.log(random);
+// The test evaluating the code below has a problem in that the instructions are 
+let num3 = 16;
+let num4 =4;
+let mod = num3/num4;
+console.log(mod);
+
+let num5 = 7;
+let num6 = 9;
+let num7 = 10;
+let num8 = 12
+let num9 = 20;
+let max = Math.max(num5,num6,num7,num8,num9);
+console.log(max);
